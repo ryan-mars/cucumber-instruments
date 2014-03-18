ba86e3810c342cbabebab066bef323094c03c54e
Ryan Marsh <ryan.marsh@mac.com>
2014-03-18 14:58:32 -0500

    Adding simple fixture app

---
+++ spec/fixtures/FixtureApp/FixtureApp/RWMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RWMAppDelegate.h
+//  FixtureApp
+//
+//  Created by Ryan Marsh on 3/18/14.
+//  Copyright (c) 2014 Ryan Marsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
