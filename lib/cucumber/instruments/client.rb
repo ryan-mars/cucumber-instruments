ac9c0bfea068406bc76074dae54e5037589bd35a
Ryan Marsh <ryan.marsh@mac.com>
2014-05-22 23:36:06 -0500

    cleaning things up

--- lib/cucumber/instruments/client.rb
+++ lib/cucumber/instruments/client.rb
@@ -4,13 +4,7 @@ module Cucumber
 module Instruments
   class Client 
     def self.uia_execute(javascript)
-      puts "SENDING: #{javascript}"
-      conn = Faraday.new(:url => 'http://localhost:8000') do |faraday|
-        faraday.response  :logger  
-        faraday.adapter   Faraday.default_adapter
-        faraday.request   :retry, max: 3, interval: 0.5,
-                          exceptions: ['Faraday::ConnectionFailed']
-      end
+      conn = Faraday.new(:url => 'http://localhost:8000')
 
       begin 
         response = conn.post do |req|
@@ -23,7 +17,6 @@ module Instruments
         retries +=  1
         if retries < 60
           sleep 0.5
-          puts "RETRYING...."
           retry
         else 
           raise e
