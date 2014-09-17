d602585100dd76dd560c3793e15d48880131b097
Ryan Marsh <ryan.marsh@mac.com>
2014-09-17 11:37:05 -0500

    Fixing tabs

--- features/step_definitions/test_drive.rb
+++ features/step_definitions/test_drive.rb
@@ -1,9 +1,9 @@
 module CucumberInstrumentsHelper
-	def install_cucumber_instruments
-		gem "cucumber-instruments", :path => "#{File.expand_path('.')}"
-	end
+  def install_cucumber_instruments
+    gem "cucumber-instruments", :path => "#{File.expand_path('.')}"
+  end
 
-	def gem(name, options)
+  def gem(name, options)
     line = %{gem "#{name}", #{options.inspect}\n}
     append_to_file('Gemfile', line)
   end
